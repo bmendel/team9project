238037391d6751dd38c20b5577cfb90a70f1f38c
Antonio Vega <antvega@csumb.edu>
2018-11-05 20:29:02 +0000

    updated functions

--- inc/functions_antonio.php
+++ inc/functions_antonio.php
@@ -1,1 +1,43 @@
-ima start today Nov 5
+<?php
+
+    // if(isset($_POST['infoId'])){
+    //     foreach($_SESSION['cart'] as $itemKey=> $item) {
+    //         if($item['id']==$_POST['removeId']){
+    //             unset($_SESSION['cart'][$itemKey]);
+    //         }
+    //     }
+    // }
+
+    function displayMovieInfo(){
+        global $dbConn;
+        $id;
+        $sql = "SELECT bb_id FROM blockbuster";
+        $stmt = $dbConn->prepare($sql);
+        $stmt->execute();
+        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo "<div>";
+        foreach ($records as $record) {
+            if($id == $record['bb_id']){
+                echo "<h2>Title: ".$record['bb_title']."</h2><br>";
+                echo "<h3>Director: ".$record['bb_director']."</h3><br>";
+                echo "<h3>Runtime: ".$record['bb_runtime']."</h3><br>";
+                echo "<h3>Genre: ".$record['bb_genre']."</h3><br>";
+                break;
+            }
+        //   echo "<option value='".$record['bb_genre']."'>".$record['bb_genre']."</option>";
+        }
+        echo "</div>";
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
