2641c76d9d50bb2ed7920a5d9b6f40080c30e197
Antonio Vega <antvega@csumb.edu>
2018-11-05 20:37:12 +0000

    pushing

--- inc/functions_antonio.php
+++ inc/functions_antonio.php
@@ -1,32 +1,27 @@
 <?php
 
-    // if(isset($_POST['infoId'])){
-    //     foreach($_SESSION['cart'] as $itemKey=> $item) {
-    //         if($item['id']==$_POST['removeId']){
-    //             unset($_SESSION['cart'][$itemKey]);
-    //         }
-    //     }
-    // }
 
     function displayMovieInfo(){
-        global $dbConn;
-        $id;
-        $sql = "SELECT bb_id FROM blockbuster";
-        $stmt = $dbConn->prepare($sql);
-        $stmt->execute();
-        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        echo "<div>";
-        foreach ($records as $record) {
-            if($id == $record['bb_id']){
-                echo "<h2>Title: ".$record['bb_title']."</h2><br>";
-                echo "<h3>Director: ".$record['bb_director']."</h3><br>";
-                echo "<h3>Runtime: ".$record['bb_runtime']."</h3><br>";
-                echo "<h3>Genre: ".$record['bb_genre']."</h3><br>";
-                break;
+        if(isset($_POST['infoId'])){
+            global $dbConn;
+            $id = $_POST['infoId'];
+            $sql = "SELECT bb_id FROM blockbuster";
+            $stmt = $dbConn->prepare($sql);
+            $stmt->execute();
+            $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            echo "<div>";
+            foreach ($records as $record) {
+                if($id == $record['bb_id']){
+                    echo "<h2>Title: ".$record['bb_title']."</h2><br>";
+                    echo "<h3>Director: ".$record['bb_director']."</h3><br>";
+                    echo "<h3>Runtime: ".$record['bb_runtime']."</h3><br>";
+                    echo "<h3>Genre: ".$record['bb_genre']."</h3><br>";
+                    break;
+                }
+            //   echo "<option value='".$record['bb_genre']."'>".$record['bb_genre']."</option>";
             }
-        //   echo "<option value='".$record['bb_genre']."'>".$record['bb_genre']."</option>";
+            echo "</div>";
         }
-        echo "</div>";
     }
 
 ?>
@@ -38,6 +33,6 @@
         <title></title>
     </head>
     <body>
-        
+        <?php displayMovieInfo(); ?>
     </body>
 </html>
