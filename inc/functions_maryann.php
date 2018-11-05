d2ace3b08b41fe97ee20131c3db742283e884509
Maryann Cortez <marcortez@csumb.edu>
2018-11-05 00:54:20 +0000

    Initial ideas for Shopping Cart

---
+++ inc/functions_maryann.php
@@ -0,0 +1,54 @@
+<?php
+
+    //connect to database
+    include '../dbConnection.php';
+    $dbConn = startConnection();
+    
+
+    $cart = array(); //array that holds cart info
+    
+    //add function
+    function addToCart(){
+        global $cart;
+        $id = $_GET['bb_id'];
+        $cart[] = $id;
+        
+    }
+    
+    //delete function
+    function deleteItem(){
+        global $cart;
+        unset($cart[$id]); //delete element
+        array_values(); //reorganizes indexes
+        
+    }
+    
+    //displays cart
+    function displayCart(){
+        global $cart;
+        
+        for($i = 0; $i < sizeof($cart); $i++){
+            displayMovieInfo($cart[$i]);
+            echo "<br><hr><br>";
+        
+        }
+    }
+    
+    
+    //displays individual movie
+    function displayMovieInfo($movie){
+        global $dbConn;
+        $sql = "SELECT * FROM blockbuster WHERE bb_id = " . $movie;
+        $stmt = $dbConn->prepare($sql);
+        $stmt->execute();
+        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($records as $record) {
+            echo $record['bb_title'] . "<br>";
+            echo "<img src='../img/". $movie .".png'> <br>";
+            echo $record['bb_year'];
+        }
+        
+    }
+
+
+?>
