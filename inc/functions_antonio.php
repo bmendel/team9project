eda7df76b280d15ae4397a9980c352092b2e1e00
Brett Stevenson <bstevenson@csumb.edu>
2018-11-06 03:02:35 +0000

    Modify info page layout

--- inc/functions_antonio.php
+++ inc/functions_antonio.php
@@ -15,23 +15,25 @@
             echo "<div id='movieInfo'>";
             foreach ($records as $record) {
                 if($id == $record['bb_id']){
-                    echo "<h2>Title: ".$record['bb_title']."</h2><br>";
-                    echo "<h3>Director: ".$record['bb_director']."</h3><br>";
-                    echo "<h3>Runtime: ".$record['bb_runtime']." minutes</h3><br>";
-                    echo "<h3>Genre: ".$record['bb_genre']."</h3><br>";
+                    echo "<img src='../img/".$record['bb_id'].".png' alt='".$record['bb_title']."'/>";
+                    echo "<div class='info-container'>";
+                    echo "<h2>".$record['bb_title']."</h2>";
+                    echo "<p>Director: ".$record['bb_director']."</p>";
+                    echo "<p>Runtime: ".$record['bb_runtime']." minutes</p>";
+                    echo "<p>Genre: ".$record['bb_genre']."</p>";
                     
                     // form 
                     echo '<form method=\'post\' action=\'../cart.php\'>';
                     echo '<input type=\'hidden\' name=\'addId\' value=\'' . $record['bb_id'] . '\'>';
-                    echo '<button>Add To Cart</button>';
+                    echo '<button class="btn-success">Add To Cart</button>';
                     echo '</form>';
                     
                     echo '<form method=\'post\' action=\'../index.php\'>';
-                    echo '<button>Back</button>';
+                    echo '<button class="btn-info">Back</button>';
                     echo '</form>';
+                    echo '</div>';
                     break;
                 }
-            //   echo "<option value='".$record['bb_genre']."'>".$record['bb_genre']."</option>";
             }
             echo "</div>";
         }
@@ -44,9 +46,13 @@
 <html>
     <head>
         <title>Movie Info</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i,800,800i"/>
         <link rel="stylesheet" type="text/css" href="../css/styles.css"/>
     </head>
-    <body>
-        <?php displayMovieInfo(); ?>
+    <body id="info">
+        <div class="page-wrapper">
+            <?php displayMovieInfo(); ?>
+        </div>
     </body>
 </html>
