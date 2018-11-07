84b6eb50252fdf7db971916049d4c7d076c6932f
Brett Stevenson <bstevenson@csumb.edu>
2018-11-07 06:43:19 +0000

    Modify cart layout and styling

--- cart.php
+++ cart.php
@@ -1,68 +1,6 @@
 <?php
-        //connect to database
     session_start();
-    include 'dbConnection.php';
-    $dbConn = startConnection();
-    
-    
-    //add function
-    function addToCart($id) {
-        foreach ($_SESSION['cart'] as $cartId) {
-            if ($id == $cartId) {
-                return;
-            }
-        }
-        array_push($_SESSION['cart'], $id);
-    }
-    
-    //delete function
-    function deleteItem($id){
-        $i = array_search($id, $_SESSION['cart']);
-        unset($_SESSION['cart'][$i]); //delete element
-        array_values($_SESSION['cart']);
-    }
-    
-    function clearCart(){
-        unset($_SESSION['cart']);
-        
-    }
-    
-    //displays cart
-    function displayCart(){
-        if(sizeof($_SESSION['cart']) == 0){
-            echo "Your Cart is Empty.<br>";
-            return;
-        }
-        foreach($_SESSION['cart'] as $movie){
-            displayMovieInfo($movie);
-            echo "<br><hr><br>";
-        }
-    }
-    
-    //displays individual movie
-    function displayMovieInfo($movie){
-        global $dbConn;
-        $sql = "SELECT * FROM blockbuster WHERE bb_id = " . $movie;
-        $stmt = $dbConn->prepare($sql);
-        $stmt->execute();
-        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($records as $record) {
-            echo $record['bb_title'] . "<br>";
-            echo "<img src='img/". $movie .".png'> <br>";
-            echo $record['bb_year'] . "<br>";
-            
-            echo "<form method='post' action='inc/functions_antonio.php'>";
-            echo "<input type='hidden' name='infoId' value ='". $movie . "'>";
-            echo "<button>Info</button>";
-            echo "</form>";
-            
-            echo "<form method='post' action='cart.php'>";
-            echo "<input type='hidden' name='deleteId' value ='". $movie . "'>";
-            echo "<button>Delete</button>";
-            echo "</form>";
-        }
-        
-    }
+    include 'inc/functions_maryann.php';
     
     if (isset($_POST['addId'])) {
         addToCart($_POST['addId']);
@@ -79,23 +17,25 @@
 <html>
     <head>
         <title>Shopping Cart </title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i,800,800i"/>
+        <link rel="stylesheet" type="text/css" href="./css/styles.css"/>
     </head>
-    <body>
-        <header><h1><strong>Your Shopping Cart</strong></h1></header>
-        
-        <?= displayCart() ?>
-        
+    <body id="cart">
+        <header><h1>Your Shopping Cart</h1></header>
+        <div class='content-wrapper'>
+            <?= displayCart() ?>
+        </div>
         <br>
+        <div class='cart-actions'>
+            <form method='post' action='index.php'>
+                <button class='btn btn-success'>Back</button>
+            </form>
         
-        <form method='post' action='index.php'>
-            <button>Back</button>
-            
-        </form>
-        
-        <form method='post'>
-            <input type='hidden' name='clearId'>
-            <button>Clear Cart</button>
-        </form>
-        
+            <form method='post'>
+                <input type='hidden' name='clearId'>
+                <button class='btn btn-danger'>Clear Cart</button>
+            </form>
+        </div>
     </body>
 </html>
